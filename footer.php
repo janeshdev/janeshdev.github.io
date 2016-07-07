b65f32a95d0042d1438da4559373f8f7d98e6c2c
Janesh Devkota <janesh.devkota@gmail.com>
2016-07-07 15:02:59 -0700

    created some php files for test

---
+++ footer.php
@@ -0,0 +1,12 @@
+<?php
+    /**-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->*/
+    echo'<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>';
+    echo '<script src="js/bootstrap.min.js"></script>';
+    echo '<script src="js/docs.js"></script>';
+	echo '</body>';
+	echo '</html>';
+
+
+?>
